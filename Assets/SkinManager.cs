99ce9f56b0713a46b756ee87fc49c0be15a4dc3c
Crazy-hermit <78893945+Crazy-hermit@users.noreply.github.com>
2022-02-26 18:22:25 +0800

    Incomplete GUI Custom menu
    added SkinManager script in the Customize scene

--- Assets/SkinManager.cs
+++ Assets/SkinManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class SkinManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public SpriteRenderer sr;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin = 0;
+    public GameObject playerskin;
+
+    public void NextSkin()
     {
-        
+        selectedSkin = selectedSkin + 1;
+
+        if (selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+        }
+
+        sr.sprite = skins[selectedSkin];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void BackSkin()
     {
-        
+        selectedSkin = selectedSkin -1;
+
+        if (selectedSkin == skins.Count)
+        {
+            selectedSkin = skins.Count - 1;
+        }
+
+        sr.sprite = skins[selectedSkin];
+    }
+
+    public void Confirm()
+    {
+        PrefabUtility.SaveAsPrefabAsset(playerskin, "Assets/Prefabs/Playables/dodger.prefab");
+        SceneManager.LoadScene("Main Menu");
     }
 }
