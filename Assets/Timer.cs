d54ba53e7f23947371843c3d9b6f7676a6cbc988
Crazy-hermit <78893945+Crazy-hermit@users.noreply.github.com>
2022-02-27 13:17:00 +0800

    timer Score
    Using a stopwatch timer to track scores

---
+++ Assets/Timer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart=0;
+    public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textbox.text = timeStart.ToString("F2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart += Time.deltaTime;
+        textbox.text = timeStart.ToString("F2");
+    }
+}
